48c7937732b98ee8e35eaeca73f80271de552ebb
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-13 20:05:08 +0600

    add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Service from './Pages/Service/Service';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-     <h1 className='text-primary'>hello</h1>
-     <button className='btn btn-secondary'>start</button>
+      <Navbar>
+  <Routes>
+    <Route path="/" element={<Home></Home>}></Route>
+    <Route path="/about" element={<About></About>}></Route>
+    <Route path="/contact" element={<Contact></Contact>}></Route>
+    <Route path="/service" element={<Service></Service>}></Route>
+    <Route path="/login" element={<Login></Login>}></Route>
+  </Routes>
+  </Navbar>
     </div>
   );
 }
